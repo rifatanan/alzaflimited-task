6a5050baf66be251989bb902bbc065ec0b96f599
rifatanan <rifatanan017@gamil.com>
2024-11-22 12:05:02 +0600

    fix backgroundimage size

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = ({data}) => {
 		<div className="container mx-auto h-[391px] flex justify-center" style={{
 				backgroundImage:`url(${hero.src})`,
 				backgroundRepeat:'no-repeat',
-				backgroundSize: '100% 391'
+				backgroundSize: '100% 391px'
 			}}>
 			<div className='w-[65%] flex justify-start relative'>
 				<Dynamic data={data} level={0}/>
